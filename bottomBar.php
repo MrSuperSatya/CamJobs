fb2be752444aa7729de1efe8d41ed441ec5c8e57
MrSuperSatya <s.sangvat@gmail.com>
2015-04-12 17:52:26 +0700

    renew

--- bottomBar.php
+++
@@ -1,9 +0,0 @@
-<div id="bottomBar">
-    <div id="subBottomBar">
-	<a href='Home'>Home</a>
-	<a href='Home'>Find Jobs</a>
-	<a href='Home'>Find Employees</a>
-	<a href='Home'>Our Services</a>
-	<a href='Home'>Contacts Us</a>
-    </div>
-</div>
